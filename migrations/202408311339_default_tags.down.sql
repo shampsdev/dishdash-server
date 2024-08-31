08508d6744f8c160f400320c034112536bbb6548
AlexDyakonov <shram.monolit@mail.ru>
2024-08-31 15:53:53 +0300

    Added migrations for adding default tags

---
+++ migrations/202408311339_default_tags.down.sql
@@ -0,0 +1,2 @@
+DELETE FROM "tag" 
+WHERE "id" IN (1, 2, 3);
---
+++ migrations/202408311339_default_tags.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO "tag" ("id", "name", "icon")
+VALUES (1, 'Бар', 'bar.png'),
+       (2, 'Кафе', 'cafe.png'),
+       (3, 'Ресторан', 'restaurant.png');
