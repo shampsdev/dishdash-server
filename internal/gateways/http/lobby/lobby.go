0213242ea0d153611621cf8bc19c3c626c9c30dd
Vaniog <forvantar@gmail.com>
2025-01-14 20:41:28 +0300

    Security update

--- internal/gateways/http/lobby/lobby.go
+++ internal/gateways/http/lobby/lobby.go
@@ -1,6 +1,7 @@
 package lobby
 
 import (
+	"dishdash.ru/internal/gateways/http/middlewares"
 	"dishdash.ru/internal/usecase"
 	"github.com/gin-gonic/gin"
 )
@@ -9,7 +10,11 @@ func SetupHandlers(r *gin.RouterGroup, cases usecase.Cases) {
 	lobbiesGroup := r.Group("lobbies")
 	lobbiesGroup.POST("", SaveLobby(cases.Lobby))
 	lobbiesGroup.GET("/:id", GetLobbyByID(cases.Lobby))
-	lobbiesGroup.DELETE("/:id", DeleteLobby(cases.Lobby))
 	lobbiesGroup.POST("/nearest", NearestLobby(cases.Lobby))
 	lobbiesGroup.POST("/find", FindLobby(cases.Lobby))
+
+	lobbiesGroupProtected := lobbiesGroup.Group("")
+	lobbiesGroupProtected.Use(middlewares.ApiTokenAuth())
+
+	lobbiesGroupProtected.DELETE("/:id", DeleteLobby(cases.Lobby))
 }
